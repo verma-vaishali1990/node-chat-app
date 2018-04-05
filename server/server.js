30fb60cbe428d476d3b81a758b9afc2ec1c1d2d3
Vaishali <vaishali.verma@taistech.com>
2018-04-05 13:21:40 +0530

    two way comminication

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const path = require('path');
-const publicPath = path.join(__dirname,'../public');
+const http = require('http');
 const express= require('express');
-var app = express();
+const socketIO = require('socket.io');
+
 var port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname,'../public');
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
-  console.log(`Listening to ${port}`);
+io.on('connection',(socket)=>{
+  console.log('New user connected');
+  socket.on('disconnect',()=>{
+    console.log('User was disconnected..');
+  });
+});
+
+server.listen(port,()=>{
+  console.log(`Server is up. Listening to ${port}`);
 });
